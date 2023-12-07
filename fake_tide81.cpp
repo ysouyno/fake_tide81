5db5dca1166c226ba291363073eaa78eeed9235c
ysouyno <ysouyno@163.com>
2023-12-07 16:18:11 +0800

    The most basic 'Tide' window can now be displayed

--- fake_tide81.cpp
+++ fake_tide81.cpp
@@ -3,35 +3,123 @@
 
 #include <Windows.h>
 
-LRESULT __stdcall
-wndproc(HWND hwnd, UINT msg, WPARAM w, LPARAM l) {
-  if (msg == WM_DESTROY) {
-    PostQuitMessage(0);
-    return 0;
-  }
-  else
-    return DefWindowProcW(hwnd, msg, w, l);
+class TideWindow {
+public:
+  TideWindow(HINSTANCE, LPSTR);
+  static void register_class(HINSTANCE h);
+  static LRESULT __stdcall twnd_proc(HWND, UINT, WPARAM, LPARAM);
+  HWND get_wnd() { return hwnd_tide; }
+
+private:
+  long wnd_proc(WORD, WPARAM, LPARAM);
+
+private:
+  static const char* class_name;
+  HWND hwnd_tide;
+
+  char window_name[MAX_PATH];
+};
+
+const char* TideWindow::class_name = NULL;
+
+TideWindow::TideWindow(HINSTANCE h, LPSTR cmd) {
+  hwnd_tide = NULL;
+  window_name[0] = '\0';
+
+  int left = 0;
+  int top = 0;
+  int width = 600;
+  int height = 400;
+
+  hwnd_tide = CreateWindowExA(WS_EX_CLIENTEDGE, class_name, window_name,
+    WS_CAPTION | WS_SYSMENU | WS_THICKFRAME |
+    WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_MAXIMIZE | WS_CLIPCHILDREN,
+    left ? left : CW_USEDEFAULT,
+    top ? top : CW_USEDEFAULT,
+    width ? width : CW_USEDEFAULT,
+    height ? height : CW_USEDEFAULT,
+    NULL, NULL, h, (LPSTR)this);
+  if (!hwnd_tide)
+    exit(false);
+
+  ShowWindow(hwnd_tide, SW_SHOWNORMAL);
 }
 
-int __stdcall
-WinMain(HINSTANCE h, HINSTANCE hprev, LPSTR cmdline, int show) {
-  WNDCLASSW wc = { 0 };
+long TideWindow::wnd_proc(WORD imsg, WPARAM wparam, LPARAM lparam) {
+  switch (imsg) {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hwnd_tide, imsg, wparam, lparam);
+  }
+
+  return 0l;
+}
+
+void TideWindow::register_class(HINSTANCE h) {
+  const char* app_name = "Tide";
+  class_name = "TideWindow";
+
+  WNDCLASSA wc = { 0 };
   wc.style = CS_HREDRAW | CS_VREDRAW;
-  wc.lpfnWndProc = wndproc;
-  wc.lpszClassName = L"unknown";
+  wc.lpfnWndProc = TideWindow::twnd_proc;
+  wc.cbWndExtra = sizeof(TideWindow*);
+  wc.hInstance = h;
+  wc.hIcon = LoadIconA(h, app_name);
+  wc.lpszMenuName = app_name;
+  wc.lpszClassName = class_name;
 
-  if (!RegisterClass(&wc))
-    return -1;
+  if (!RegisterClassA(&wc))
+    exit(false);
+}
 
-  HWND hwnd = CreateWindowW(L"unknown", L"", WS_OVERLAPPEDWINDOW,
-    0, 0, 600, 400, NULL, NULL, h, 0);
-  ShowWindow(hwnd, SW_SHOW);
-  UpdateWindow(hwnd);
+LRESULT __stdcall
+TideWindow::twnd_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+  TideWindow* tide = (TideWindow*)GetWindowLong(hwnd, 0);
+  if (!tide) {
+    if (msg == WM_CREATE) {
+      LPCREATESTRUCT cs = (LPCREATESTRUCT)lparam;
+      tide = (TideWindow*)cs->lpCreateParams;
+      tide->hwnd_tide = hwnd;
+      SetWindowLong(hwnd, 0, (LONG)tide);
+      return tide->wnd_proc(msg, wparam, lparam);
+    }
+    else
+      return DefWindowProc(hwnd, msg, wparam, lparam);
+  }
+  else
+    return tide->wnd_proc(msg, wparam, lparam);
+}
 
+int __stdcall WinMain(
+  _In_ HINSTANCE hInstance,
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ LPSTR lpCmdLine,
+  _In_ int nShowCmd
+) {
+  HACCEL h_acc_table = LoadAcceleratorsA(hInstance, "ACCELS");
+
+  TideWindow::register_class(hInstance);
+  TideWindow main_wnd(hInstance, lpCmdLine);
+
+  bool going = true;
   MSG msg;
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessageA(&msg);
+  msg.wParam = 0;
+  while (going) {
+    going = GetMessage(&msg, NULL, 0, 0);
+    if (going) {
+      if (TranslateAccelerator(msg.hwnd, h_acc_table, &msg) == 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+      }
+    }
+    else if (going) {
+      if (TranslateAccelerator(main_wnd.get_wnd(), h_acc_table, &msg) == 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+      }
+    }
   }
 
   return (int)msg.wParam;
---
+++ resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by fake_tide81.rc
+//
+#define IDM_BUILD                       40001
+#define IDM_COMPILE                     40002
+#define IDM_FIND                        40003
+#define IDM_FINDNEXT                    40004
+#define IDM_GO                          40005
+#define IDM_GOTO                        40006
+#define IDM_LWRCASE                     40007
+#define IDM_NEW                         40008
+#define IDM_NEXTFILE                    40009
+#define IDM_NEXTMSG                     40010
+#define IDM_OPEN                        40011
+#define IDM_PREVMSG                     40012
+#define IDM_REPLACE                     40013
+#define IDM_SAVE                        40014
+#define IDM_STEP                        40015
+#define IDM_STOPEXECUTE                 40016
+#define IDM_UNDO                        40017
+#define IDM_UPRCASE                     40018
+#define IDM_VIEWSPACE                   40019
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
