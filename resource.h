0fed36f50e1b267cfe969245d0c254ece108000d
ysouyno <ysouyno@163.com>
2023-12-11 14:10:33 +0800

    resource Menu and Icon added

--- resource.h
+++ resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by fake_tide81.rc
 //
+
 #define IDM_BUILD                       40001
 #define IDM_COMPILE                     40002
 #define IDM_FIND                        40003
@@ -21,13 +22,29 @@
 #define IDM_UNDO                        40017
 #define IDM_UPRCASE                     40018
 #define IDM_VIEWSPACE                   40019
+#define IDM_CLOSE                       40020
+#define IDM_SAVEAS                      40021
+#define IDM_QUIT                        40022
+#define IDM_REDO                        40023
+#define IDM_CUT                         40024
+#define IDM_COPY                        40025
+#define IDM_PASTE                       40026
+#define IDM_CLEAR                       40027
+#define IDM_SELECTALL                   40028
+#define IDM_FINDINFILES                 40029
+#define IDM_VERTICALSPLIT               40030
+#define IDM_STOPEXECUTING               40031
+#define IDM_STOPEXECUE                  40032
+#define IDM_OPENLOCALPROPERTIES         40033
+#define IDM_OPENGLOBALPROPERTIES        40034
+#define IDM_ABOUT                       40035
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40064
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
